d84e9bba73fb25258d52c35b513bfc54ccc1286e
ravishankar <ravishankarste@gmail.com>
2024-04-07 11:42:51 +0100

    latest

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,15 +1,24 @@
-﻿namespace TestProject1;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+
+namespace TestProject1;
 
 public class Tests
 {
+    IWebDriver driver;
     [SetUp]
     public void Setup()
     {
+        driver.Navigate().GoToUrl("https://wwww.saucedome.com/");
     }
 
     [Test]
     public void Test1()
     {
-        Assert.Pass();
+        Assert.IsTrue(driver.Url.Contains("saucedmoe"));
+        Assert.That( ("saucedmoe"));
+        //Assert.Pass();
+        //dadadfadfadfadfadf
     }
 }
